7e242302614fd5f2e259dc78777b2777f42ecf10
Gouranga Ghosh <gouranga.ghosh@calsoftinc.com>
2026-05-04 19:06:10 +0530

    first commit

---
+++ src/main/java/com/calsoft/api/TriggerNotificationApiApplication.java
@@ -0,0 +1,13 @@
+package com.calsoft.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriggerNotificationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriggerNotificationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calsoft/api/TriggerNotificationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calsoft.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriggerNotificationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
