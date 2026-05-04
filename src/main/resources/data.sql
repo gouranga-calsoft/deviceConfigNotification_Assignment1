991a5b855780e327b6ce460ccbd7edae1f068b25
Gouranga Ghosh <gouranga.ghosh@calsoftinc.com>
2026-05-04 20:58:23 +0530

    Added ignored files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Devices (device_ip, device_details, config_changed) VALUES ('192.168.1.10', 'Router', false);
+INSERT INTO Devices (device_ip, device_details, config_changed) VALUES ('192.168.1.11', 'Switch', true);
+INSERT INTO Devices (device_ip, device_details, config_changed) VALUES ('192.168.1.12', 'Firewall', true);
+INSERT INTO Devices (device_ip, device_details, config_changed) VALUES ('192.168.1.13', 'Server', false);
